3dede7eba1616d4a29bd80764bc3ea14df8d2e5e
Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2025-10-24 06:23:10 +0600

    facing some errors

---
+++ src/app/(site)/page.tsx
@@ -0,0 +1,9 @@
+import Title from "./landing/title";
+
+export default function Home() {
+  return (
+    <main className="">
+      <Title text="A Better Way to Manage Your Studies" />
+    </main>
+  );
+}
